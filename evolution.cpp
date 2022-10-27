b00725bd712ebe11c067ab0b7672cdfa00832a43
Porter Peterson <59668589+porter-peterson@users.noreply.github.com>
2022-10-27 01:03:28 -0600

    starting point

---
+++ evolution.cpp
@@ -0,0 +1,29 @@
+
+class Creature {
+    private:
+        Creature() {
+            // Name
+            
+        };
+
+        Creature reproduce(Creature c1, Creature c2) {
+            // return new creature based on other creatures
+        };
+
+};
+
+class Actions {
+
+};
+
+class World {
+
+    Creature creature;
+
+};
+
+
+int main() {
+    
+    return 0;
+}
